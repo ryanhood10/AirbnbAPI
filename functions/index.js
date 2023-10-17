bfa001668adeb59aef179339f9261b624915c36a
Ryan Hood <ryanhood@ryans-mbp.lan>
2023-10-17 15:54:37 -0500

    firebase deployment

---
+++ functions/index.js
@@ -0,0 +1,14 @@
+// functions/index.js
+
+// Import the required modules and initialize an app
+const { logger } = require("firebase-functions");
+const { onRequest } = require("firebase-functions/v2/https");
+const { onDocumentCreated } = require("firebase-functions/v2/firestore");
+
+// The Firebase Admin SDK to access Firestore.
+const { initializeApp } = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore");
+
+initializeApp();
+
+// Your Cloud Functions code can go here...
